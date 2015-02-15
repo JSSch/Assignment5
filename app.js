ae0d20dec86f7c46d02ef5c0776437e33051d755
JSSch <smartblue@gmail.com>
2015-02-15 01:22:32 -0500

    On to Chapter 4
    Still won't live submit from form to list of reviews, suspect this will
    be implemented along with services later.

--- app.js
+++ app.js
@@ -27,7 +27,9 @@
       app.controller('ReviewController', function(){
         this.review = {};
         this.addReview = function(product) {
+            this.review.createdOn = Date.now();
             product.reviews.push(this.review);
+            
             this.review = {};
         };
     });
